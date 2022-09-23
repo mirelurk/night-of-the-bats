c8795432d68ab2ebf14230f25dd86d13d32d4109
kaijugroup <52469659+kaijugroup@users.noreply.github.com>
2022-09-22 23:30:28 -0500

    Add files via upload

---
+++ notb.py
@@ -0,0 +1,4 @@
+#Text based adventure game use SQLite as the backend to store data.  
+#Need to create an interface with which the player will interact.
+#Use the database to store the text files that will appear on the screen.
+#Create and store a local file that can be used to load a player's progress.
