5145cbff07f4e28132bd8388873512a101fb3a80
unknown <bean.kristen87@gmail.com>
2022-09-23 22:16:26 -0500

    adding initial file structure

--- notb.py
+++ notb.py
@@ -2,3 +2,6 @@
 #Need to create an interface with which the player will interact.
 #Use the database to store the text files that will appear on the screen.
 #Create and store a local file that can be used to load a player's progress.
+
+def main():
+    do nothing
