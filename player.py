1304a999168d1271321cd53015f07f2bc223cd7c
unknown <bean.kristen87@gmail.com>
2022-10-08 23:52:16 -0500

    Added initial inventory functions.

--- player.py
+++ player.py
@@ -5,7 +5,25 @@ class Player:
         self.health = 50 #initial health
         self.xp = 0 #starts at 0 xp
         self.loc = 1 #starting location for a new game
+        self.inv = []
 
     def status(self):
         status = str("Health: "+self.health+"XP: "+self.xp)
         print(status)
+
+    def updatehealth(self, n):
+        currhealth = self.health
+        self.health = currhealth + n
+
+    def printinv(self):
+        for i in self.inv:
+            print(i)
+
+    def dropinv(self, item):
+        itemi = self.inv.index(item)
+        self.inv.pop(itemi)
+        return(1)
+    
+    def addinv(self, item):
+        self.inv.append(item)
+        return(1)
